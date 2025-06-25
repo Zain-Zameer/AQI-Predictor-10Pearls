f6c1de85ac850fdc8ffa261466c83051fbc34ce7
Zain-Zameer <officialmuhammadzain45@gmail.com>
2025-06-26 01:58:49 +0500

    added

---
+++ Scripts/getWeather.py
@@ -0,0 +1,57 @@
+import requests
+import json
+import csv
+import os
+
+# Your API key
+API_KEY = "d0880b1c-e430-4eba-8bcb-68c6605e7a6e"
+
+# Nearest city based on IP
+url = f"https://api.airvisual.com/v2/nearest_city?key={API_KEY}"
+
+# Make the request
+response = requests.get(url)
+
+# Handle the response
+if response.status_code == 200:
+    data = response.json()
+    
+    if data["status"] != "success":
+        print("API returned failure status.")
+        exit()
+
+    pollution = data["data"]["current"]["pollution"]
+    weather = data["data"]["current"]["weather"]
+    location = data["data"]["location"]
+
+    row = {
+        "timestamp": pollution["ts"],
+        "city": data["data"]["city"],
+        "state": data["data"]["state"],
+        "country": data["data"]["country"],
+        "latitude": location["coordinates"][1],
+        "longitude": location["coordinates"][0],
+        "aqius": pollution["aqius"],
+        "mainus": pollution["mainus"],
+        "aqicn": pollution["aqicn"],
+        "maincn": pollution["maincn"],
+        "temperature": weather["tp"],
+        "humidity": weather["hu"],
+        "pressure": weather["pr"],
+        "wind_direction": weather["wd"],
+        "wind_speed": weather["ws"]
+    }
+    print(row)
+    
+    with open("../Data/raw/weather_init.csv","a") as f:
+        f.write("\n")
+        for key in row:
+            if(key!="wind_speed"):
+                f.write(f'{row[key]},')
+            else:
+                f.write(f'{row[key]}')
+    print("written successfully")
+    
+
+else:
+    print(f"❌ Error {response.status_code}: {response.text}")
